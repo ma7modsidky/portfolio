e2eabf8531153dd37727a5b9c908e6f7a544f55d
Mahmoud <ma7mod_a@live.com>
2021-04-21 20:03:40 -0700

    about me modification

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,8 +6,9 @@ function Home() {
         <div className="home">
             <div className='about_me'>
                 <h3 className='home_title'>About Me</h3>
-                <p>Started earnest brother believe an exposed so. Me he believing daughters if forfeited at furniture. Age again and stuff downs spoke. Late hour new nay able fat each sell. Nor themselves age introduced frequently use unsatiable devonshire get. They why quit gay cold rose deal park. One same they four did ask busy. Reserved opinions fat him nay position. Breakfast as zealously incommode do agreeable furniture. One too nay led fanny allow plate. </p>
-                <p>Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may. </p>
+                <p>Hi There I'm Mahmoud , I'm a full stack web developer with 2 years of experience.</p>
+                <p>I worked as a doctor before, but I found my passion and love for coding and web development, so I decided to switch my career and do the most thing I'm good at </p>
+                <p>I'm glad to work with you in the future and deliver you the best work you want</p>
             </div>
             <div className='my_service'>
                 <h3 className='home_title'>My Services</h3>
